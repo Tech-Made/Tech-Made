99b464bbe5608c8edd66a182ac818f783b38bc62
Asim Zaidi <asimzaidih@gmail.com>
2018-11-27 22:28:06 -0800

    deleted old files, heroku port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,5 +44,10 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// for heroku
+const port = process.env.PORT || 3000;
+app.listen(port);
+
 
 module.exports = app;
