6c0a2e712cd3ab7e471d09dff4420a24ce13ce1a
Asim Zaidi <asimzaidih@gmail.com>
2018-12-07 14:25:20 -0800

    :bug: fixed dashboard post

--- routes/project.js
+++ routes/project.js
@@ -1,24 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
 const User = require("../models/user");
 const Project = require("../models/project");
 
 router.post('/dashboard', function(req, res, next) {
-    console.log("Req User _id:", req.user._id);
-    
-    User.findById(req.user._id).then(user => {
-        console.log("user found:", user);
-        const project = new Project(req.body);
-        console.log("project:", project);
-        project.stage += 1;
-        user.projects.push(project._id);
-        user.fullName = "CHANGED"
-        console.log("user updated:", user);
-    }).then(e => {
-        console.log("rendering dashboard");
-        
-        res.redirect('dashboard')
+    const project = new Project(req.body);
+    project.save().then(() => {
+        User.findByIdAndUpdate(req.user._id, { $push: { projects: project._id } })
+        .then(user => {
+            res.redirect('/dashboard');
+        });
     });
-  });
+});
   
 module.exports = router;
