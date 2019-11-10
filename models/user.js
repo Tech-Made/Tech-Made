02f1efdaca108d480b6bfd9f1d435a8dc2b070ec
Asim Zaidi <asimzaidih@gmail.com>
2019-11-10 16:39:24 -0600

    footer, working on contact route

--- models/user.js
+++ models/user.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    fullName: { type: String },
-    email: { type: String, required: true },
-    hometown: String
+  name: String,
+  email: String,
+  phone: String,
+  message: String,
 }, {
   timestamps: true,
 });
-  
+
 module.exports = mongoose.model("User", UserSchema);
-  
